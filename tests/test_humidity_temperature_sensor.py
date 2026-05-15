82831e07cb0a29efeec1184cbc3d652ab038dd68
Patrick Schaffner <patrick.schaffner@protonmail.com>
2026-05-15 19:50:49 +0200

    Export HumidityTemperatureSensor from gpiozero package

--- tests/test_humidity_temperature_sensor.py
+++ tests/test_humidity_temperature_sensor.py
@@ -250,3 +250,9 @@ def test_when_activated_fires(tmp_path, mock_factory):
         assert not s.is_active
         (Path(device) / 'in_humidityrelative_input').write_text('90000')
         assert fired.wait(timeout=5), 'when_activated did not fire'
+
+
+def test_exported_from_package():
+    import gpiozero
+    from gpiozero.internal_devices import HumidityTemperatureSensor as Internal
+    assert gpiozero.HumidityTemperatureSensor is Internal
